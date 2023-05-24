41f296222217420be2a42d195112ca1cd58a4ecd
Facundo Venturi <81877086+fakuventuri@users.noreply.github.com>
2023-05-24 14:55:38 -0300

    Hero text size fix

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -2,10 +2,10 @@ export default function Hero() {
   return (
     <div className="flex items-center justify-center w-screen h-full px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center justify-center text-center max-w-7xl">
-        <h1 className="text-5xl font-bold tracking-tight text-white md:text-6xl lg:text-8xl">
+        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl lg:text-8xl whitespace-nowrap">
           Facundo Venturi
         </h1>
-        <p className="max-w-2xl mx-auto mt-5 text-lg text-gray-300 md:text-xl lg:text-2xl">
+        <p className="max-w-2xl mx-auto mt-5 text-base text-gray-300 sm:text-xl lg:text-2xl">
           Software, Web FullStack & Native App Developer.
         </p>
       </div>

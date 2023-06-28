56d186b6d1dff91fbdf496667fdee1bcafc253a6
Facundo Venturi <venturifaku@gmail.com>
2023-06-28 15:59:28 -0300

    Update projectView.tsx

--- components/projectView.tsx
+++ components/projectView.tsx
@@ -3,7 +3,7 @@ import Project from "@/types/project";
 export default function ProjectView({ project }: { project: Project }) {
   return (
     <div>
-      <h1>ProjectView</h1>
+      <h1>Under construction</h1>
     </div>
   );
 }

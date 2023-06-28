b0c1317136677596d6222f1a22e71753de5fa78a
Facundo Venturi <venturifaku@gmail.com>
2023-06-28 14:37:44 -0300

    Project view configure

---
+++ components/projectView.tsx
@@ -0,0 +1,9 @@
+import Project from "@/types/project";
+
+export default function ProjectView({ project }: { project: Project }) {
+  return (
+    <div>
+      <h1>ProjectView</h1>
+    </div>
+  );
+}
